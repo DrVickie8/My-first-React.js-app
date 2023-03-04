7871c4db3046866826581fa65a495eb188474ba2
VICTORIA JOHNSON <110999245+DrVickie8@users.noreply.github.com>
2023-03-04 01:55:31 -0800

    Modifications

--- src/App.js
+++ src/App.js
@@ -4,20 +4,9 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
+    <header className="App-header">
+        
+      </header>
       <h1>My favourite hobby is watching YouTube cooking videos😊</h1>
       <p>This is because I love food</p>
     </div>
