911c1d900fe6f1617abdb167e16624bbcd361c76
VICTORIA JOHNSON <110999245+DrVickie8@users.noreply.github.com>
2023-03-04 02:09:56 -0800

    Modifying existing components

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-    <header className="App-header">
-        
+      <header className="App-header">
+        <h1>My favourite hobby is watching YouTube cooking videos😊</h1>
+        <p>This is because I love food. They include: </p>
+        <ul>
+          <li>Amala and Abula</li>
+          <li>Peppered chicken</li>
+          <li>Vegetable sauce with white rice</li>
+        </ul>
       </header>
-      <h1>My favourite hobby is watching YouTube cooking videos😊</h1>
-      <p>This is because I love food</p>
     </div>
   );
 }
